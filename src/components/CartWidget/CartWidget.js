e3ee8e6bff0941910342a9522156c7f3a3d3d804
Jacobo Tomas Lerner Rojas <j.lernerrojas@gmail.com>
2023-04-22 11:56:14 -0300

    Solucion error navbar y cambio de estilo contador carrito

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -3,8 +3,8 @@ import cart from "./assets/cart.png"
 const CartWidget =()=>{
     return(
         <div>
-            <img src={cart} alt="carrito-widget" de compras />
-            0
+            <img src={cart} alt="carrito-widget" de compras/>
+            <span className="badge text-bg-secondary">{0}</span>
         </div>
     )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,11 +13,11 @@ const Navbar =()=>{
             <div className="container-fluid">
                 <img src={logo2} alt="Logo" width="40" height="24" className="d-inline-block align-text-top"/>
                 <a className="navbar-brand bg-body-tertiary p-2 text-uppercase fw-bolder titulo" href="#">La Tienda del Basket</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse d-flex justify-content-end align-items-center" id="navbarNav" >
-                    <ul className="navbar-nav ">
+                <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
                         <li className="nav-item p-2 ">
                             <button {...botone} >Camisetas</button>
                         </li>
@@ -27,10 +27,8 @@ const Navbar =()=>{
                         <li className="nav-item p-2">
                             <button {...botone}>Accesorios</button>
                         </li>
-                        <li className="nav-item px-3">
-                            <CartWidget/>
-                        </li>
-                    </ul>    
+                    </ul>  
+                    <CartWidget/>   
                 </div>               
             </div>
         </nav>
